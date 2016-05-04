86b00e6a710db05cd36e6c1b8838c700c0e03c02
yoqo <kk1346@qq.com>
2016-05-04 23:20:51 +0800

    五子棋游戏优化
    棋子高光

--- game/wuziqi/js/script.js
+++ game/wuziqi/js/script.js
@@ -106,7 +106,7 @@ var oneStep = function(i, j, me) {
 	context.beginPath();
 	context.arc(15 + i*30, 15 + j*30, 13, 0, 2 * Math.PI);
 	context.closePath();
-	var gradient = context.createRadialGradient(15 + i*30 + 2, 15 + j*30 - 2, 13, 15 + i*30 + 2, 15 + j*30 - 2, 3);
+	var gradient = context.createRadialGradient(15 + i*30 - 2, 15 + j*30 - 2, 13, 15 + i*30 - 2, 15 + j*30 - 2, 4);
 	if(me){
 		gradient.addColorStop(0, "#0a0a0a");
 		gradient.addColorStop(1, "#636766");
